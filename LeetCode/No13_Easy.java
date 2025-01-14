80c455609c0b8da34654a3421b49c2de8020a127
DavidChen0428 <davidchen428@yahoo.com>
2025-01-14 23:25:25 +0800

    No13    1.字串轉字元陣列         2.使用switch...case將字串對應數字         3.使用for迴圈對數字做比較         4.相加相減得總數

---
+++ LeetCode/No13_Easy.java
@@ -0,0 +1,45 @@
+package LeetCode;
+
+public class No13_Easy {
+    // 題型條件:
+    // 羅馬數字的規律 -> 
+    // 1.順序不會亂MDCLXVI，但前後位可能會對調
+    // 2.前位數字比後位數字小要相減
+    // 3.後位數字比前位數字小要相加
+    public int romanToInt(String s) {
+        char ch[] = s.toCharArray();
+        int count = 0;
+        for (int i = 0; i < ch.length - 1; i++) {
+            int current = getRomanValue(ch[i]);
+            int next = getRomanValue(ch[i + 1]);
+            if (current < next) {
+                count -= current;
+            } else {
+                count += current;
+            }
+        }
+        // Add the value of the last character
+        count += getRomanValue(ch[ch.length - 1]);
+        return count;
+    }
+    private int getRomanValue(char c) {
+        switch (c) {
+            case 'I':
+                return 1;
+            case 'V':
+                return 5;
+            case 'X':
+                return 10;
+            case 'L':
+                return 50;
+            case 'C':
+                return 100;
+            case 'D':
+                return 500;
+            case 'M':
+                return 1000;
+            default:
+                return 0;
+        }
+    }
+}
