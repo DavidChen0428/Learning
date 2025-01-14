ed18635a435c4c2dc6ff750df7b286d6c3c99e8c
DavidChen0428 <davidchen428@yahoo.com>
2025-01-14 23:38:25 +0800

    update complete

---
+++ LeetCode/No14_Easy.java
@@ -0,0 +1,26 @@
+package LeetCode;
+
+public class No14_Easy {
+    // 題目條件:
+    // 給定一個字串陣列 陣列裡符合每個元素的最長標頭(prefix)
+    public static String longestCommonPrefix(String[] strs) {
+        if(strs==null||strs.length==0){
+            return "";
+        }
+        String prefix=strs[0];
+        for(int i=1;i<strs.length;i++){
+            while(strs[i].indexOf(prefix)!=0){
+                prefix=prefix.substring(0,prefix.length()-1);
+                if(prefix.isEmpty()){
+                    return "";
+                }
+            }
+        }
+        return prefix;
+    }
+
+    public static void main(String[] args) {
+        String[] strs={"flower","flow","flight"};
+        System.out.println(longestCommonPrefix(strs));
+    }
+}
