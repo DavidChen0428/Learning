ef7088666cc060c94a6413efe3c55628a65ef665
DavidChen0428 <davidchen428@yahoo.com>
2025-02-22 01:42:22 +0800

    20250222_update

--- JavaOCP/Graphics.java
+++ JavaOCP/Graphics.java
@@ -1,6 +1,6 @@
 public class Graphics {
     // 正方形
-    public static void square(int length){
+    public static void getSquare(int length){
         for(int i=1;i<=length;i++){
             for(int j=1;j<=length;j++){
                 System.out.print("* ");
@@ -9,8 +9,18 @@ public class Graphics {
         }
     }
 
+    // 矩形
+    public static void getRectangle(int width,int length){
+        for(int i=1;i<=width;i++){
+            for(int j=1;j<=length;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+
     // 左下直角三角形
-    public static void rightTriangle1(int length){
+    public static void getRightTriangle1(int length){
         for(int i=1;i<=length;i++){
             for(int j=1;j<=i;j++){
                 System.out.print("* ");
@@ -20,7 +30,7 @@ public class Graphics {
     }
 
     // 左上直角三角形
-    public static void rightTriangle2(int length){
+    public static void getRightTriangle2(int length){
         for(int i=1;i<=length;i++){
             for(int j=1;j<=length-i+1;j++){
                 System.out.print("* ");
@@ -30,7 +40,7 @@ public class Graphics {
     }
     
     // 右上直角三角形
-    public static void rightTriangle3(int length){
+    public static void getRightTriangle3(int length){
         for(int i=1;i<=length;i++){
             for(int j=1;j<=length;j++){
                 if(j<i){
@@ -44,7 +54,7 @@ public class Graphics {
     }
 
     // 右下直角三角形
-    public static void rightTriangle4(int length){
+    public static void getRightTriangle4(int length){
         for(int i=1;i<=length;i++){
             for(int j=length;j>i;j--){
                 System.out.print("  ");
@@ -56,9 +66,75 @@ public class Graphics {
         }
     }
 
+    // 等腰三角形
+    public static void getIsoscelesTriangle1(int height){
+        for(int i=0;i<height;i++){
+            for(int j=height-i;j>1;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=2*i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // 倒等腰三角形
+    public static void getIsoscelesTriangle2(int height){
+        for(int i=height-1;i>=0;i--){
+            for(int j=height-i;j>1;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=2*i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // 菱形 : 兩個等腰三角形
+    public static void getDiamond(int halfHeight){
+        for(int i=0;i<halfHeight;i++){
+            for(int j=halfHeight-i;j>1;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=i*2;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+        for(int i=halfHeight-2;i>=0;i--){
+            for(int j=halfHeight-i;j>1;j--){
+                System.out.print(" ");
+            }
+            for(int j=0;j<=2*i;j++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
+    // 梯形
+    public static void getTrapezoid(int height, int topWidth, int bottomWidth){
+        for(int i=0;i<height;i++){
+            int currentWidth=topWidth+(bottomWidth-topWidth)*i/(height-1);
+            int space=(bottomWidth-currentWidth)/2;
+            for(int j=0;j<space;j++){
+                System.out.print(" ");
+            }
+            for(int k=0;k<currentWidth;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+
     public static void main(String[] args) {
-        //square(6);
-        //rightTriangle1(4);
-        
+        //getSquare(6);
+        //getRightTriangle1(4);
+        //getRectangle(4, 5);
+        //getTrapezoid(5, 3, 9);
+        //getIsoscelesTriangle2(5);
+        getDiamond(5);
     }
 }
