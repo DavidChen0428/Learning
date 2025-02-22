4a81940b4896172d5982337f145571bf53e3b7d7
DavidChen0428 <davidchen428@yahoo.com>
2025-02-22 23:28:23 +0800

    20250222_update

--- JavaOCP/ForOrForEach.java
+++ JavaOCP/ForOrForEach.java
@@ -16,6 +16,7 @@ public class ForOrForEach{
 
         // 使用 foreach 迴圈遍歷 List
         // 特色 : 簡單且直觀
+        System.out.println("使用 foreach 迴圈遍歷 List");
         for(String fruit:fruitsList){
             System.out.println(fruit);
         }
@@ -23,6 +24,7 @@ public class ForOrForEach{
         System.out.println("==========");
         // 使用 iterator 遍歷 List
         // 特色 : 更靈活，適合需要在迭代過程中修改集合的情況
+        System.out.println("使用 iterator 遍歷 List");
         Iterator<String> iterator1=fruitsList.iterator();
         while(iterator1.hasNext()){
             String fruit=iterator1.next();
@@ -38,12 +40,14 @@ public class ForOrForEach{
         fruitsSet.add("Cherry");
 
         // 使用 foreach 迴圈遍歷 List
+        System.out.println("使用 foreach 迴圈遍歷 List");
         for(String fruit: fruitsSet){
             System.out.println(fruit);
         }
 
         System.out.println("==========");
         // 使用 iterator 遍歷 List
+        System.out.println("使用 iterator 遍歷 List");
         Iterator<String> iterator2=fruitsSet.iterator();
         while(iterator2.hasNext()){
             String fruit=iterator2.next();
@@ -59,20 +63,50 @@ public class ForOrForEach{
         fruitColors.put("Cherry", "Red");
 
         // 使用 foreach 迴圈遍歷 keySet
+        System.out.println("使用 foreach 迴圈遍歷 keySet");
         for(String fruit: fruitColors.keySet()){
             System.out.println(fruit);
         }
 
         System.out.println("==========");
         // 使用 foreach 迴圈遍歷 values
+        System.out.println("使用 foreach 迴圈遍歷 values");
         for(String fruit: fruitColors.values()){
             System.out.println(fruit);
         }
 
         System.out.println("==========");
         // 使用 foreach 迴圈遍歷 entrySet
+        System.out.println("使用 foreach 迴圈遍歷 entrySet");
         for(Map.Entry<String,String> entry:fruitColors.entrySet()){
             System.out.println("Fruit: "+entry.getKey()+", Color: "+entry.getValue());
+        }
+
+        System.out.println("==========");
+        // 使用 iterator 遍歷 keySet
+        System.out.println("使用 iterator 遍歷 keySet");
+        Iterator<String> iterator3=fruitColors.keySet().iterator();
+        while(iterator3.hasNext()){
+            String key=iterator3.next();
+            System.out.println("Key: "+key+", Value: "+fruitColors.get(key));
+        }
+
+        System.out.println("==========");
+        // 使用 iterator 遍歷 values
+        System.out.println("使用 iterator 遍歷 values");
+        Iterator<String> iterator4=fruitColors.values().iterator();
+        while(iterator4.hasNext()){
+            String value=iterator4.next();
+            System.out.println("Value: "+value);
+        }
+
+        System.out.println("==========");
+        // 使用 iterator 遍歷 entrySet
+        System.out.println("使用 iterator 遍歷 entrySet");
+        Iterator<Map.Entry<String,String>> iterator5=fruitColors.entrySet().iterator();
+        while(iterator5.hasNext()){
+            Map.Entry<String,String> entry=iterator5.next();
+            System.out.println("Key: "+entry.getKey()+", Value: "+entry.getValue());
         }
     }
 }
